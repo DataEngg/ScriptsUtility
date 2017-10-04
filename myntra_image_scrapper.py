142b5dd70cef440ccfe9c8ad843cbcb1dd1a661b
DataEngg <kunal1340@outlook.com>
2017-10-04 11:48:14 +0530

    Added saree and shirt link

--- myntra_image_scrapper.py
+++ myntra_image_scrapper.py
@@ -33,8 +33,34 @@ class ImageScrapper(object):
                         rneck_images.append((i))
         df = pd.DataFrame(data=rneck_images) 
         df.to_csv("rneck_links.csv")
+    
+    
+    def saree(self):
+        saree = []
+        for i in range(1,20):
+            json_value = json.loads(requests.get(str("https://www.myntra.com/web/v2/search/data/saree?p={0}&rows=48").format(i)).text)
+            for key in json_value['data']['results']['products']:
+                for k ,i in key.items():
+                    if k == 'search_image':
+                        saree.append((i))
+        df = pd.DataFrame(data=saree)
+        df.to_csv("saree_links.csv")
+
+
+    def shirts(self):
+        shirts = []
+        for i in range(1,20):
+            json_value = json.loads(requests.get(str("https://www.myntra.com/web/v2/search/data/shirts?p={0}&rows=48").format(i)).text)
+            for key in json_value['data']['results']['products']:
+                for k ,i in key.items():
+                    if k == 'search_image':
+                        shirts.append((i))
+        df = pd.DataFrame(data=shirts)
+        df.to_csv("shirts_links.csv")
 
 if __name__ == '__main__':
     ims = ImageScrapper()
-    ims.v_neck()
+    ims.saree()
+    ims.shirts()
+    #ims.v_neck()
     #ims.round_neck()
